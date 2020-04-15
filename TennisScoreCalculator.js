2b445c6a52ce6c89831112a01dded8b166a42b7b
andys <andysmith836@gmail.com>
2020-04-15 21:05:17 +0100

    Player 2 can noew score

--- TennisScoreCalculator.js
+++ TennisScoreCalculator.js
@@ -2,6 +2,9 @@ function score(player1Points, player2Points) {
     if (player1Points === 1) {
         return "Fifteen-Love";
     }
+    if (player2Points === 1) {
+        return "Love-Fifteen";
+    }
     return "Love-Love";
 }
 
--- TennisScoreCalculator.spec.js
+++ TennisScoreCalculator.spec.js
@@ -4,5 +4,6 @@ describe("Tennis Calculator", () => {
     it("should score simple scores", () => {
         expect(score(0, 0)).toEqual("Love-Love");
         expect(score(1, 0)).toEqual("Fifteen-Love");
+        expect(score(0, 1)).toEqual("Love-Fifteen");
     });
 });
