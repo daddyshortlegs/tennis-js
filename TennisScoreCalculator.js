160d5f60d0d1b18aea2b7872f31bf715cf5ee87d
andys <andysmith836@gmail.com>
2020-04-15 22:37:19 +0100

    Clean up

--- TennisScoreCalculator.js
+++ TennisScoreCalculator.js
@@ -1,12 +1,8 @@
 function score(player1Points, player2Points) {
-
-    let maxScore = Math.max(player1Points, player2Points);
-
-    if (maxScore >= 4) {
+    if (isWinOrAdvantage(player1Points, player2Points)) {
         if (player1Points !== player2Points) {
             const delta = Math.abs(player1Points - player2Points);
-            const playerName = (player1Points > player2Points ? "1" : "2");
-            return (delta > 1) ? `Win for player ${playerName}` : `Advantage player ${playerName}`;
+            return `${winOrAdvantage(delta)} player ${winningPlayer(player1Points, player2Points)}`;
         }
     }
 
@@ -15,6 +11,18 @@ function score(player1Points, player2Points) {
     }
 
     return scoreFor(player1Points) + "-" + scoreFor(player2Points);
+}
+
+function isWinOrAdvantage(player1Points, player2Points) {
+    return Math.max(player1Points, player2Points) >= 4;
+}
+
+function winOrAdvantage(delta) {
+    return (delta > 1) ? `Win for` : `Advantage`;
+}
+
+function winningPlayer(player1Points, player2Points) {
+    return (player1Points > player2Points ? "1" : "2");
 }
 
 function scoreFor(points) {
