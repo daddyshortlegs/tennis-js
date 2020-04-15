a581aa9fe08077a220e3da0825f6202012d8ffcd
andys <andysmith836@gmail.com>
2020-04-15 22:23:26 +0100

    Slight refactor

--- TennisScoreCalculator.js
+++ TennisScoreCalculator.js
@@ -1,38 +1,34 @@
 function score(player1Points, player2Points) {
-    const delta = Math.abs(player1Points - player2Points);
-    if (player1Points >= 4) {
+
+    let maxScore = Math.max(player1Points, player2Points);
+
+    if (maxScore >= 4) {
         if (player1Points > player2Points) {
-            let playerName = "1";
-            if (delta > 1) {
-                return `Win for player ${playerName}`;
-            } else if (delta === 1) {
-                return `Advantage player ${playerName}`;
-            } else {
-                return;
-            }
+            return decideWinner(player1Points, player2Points);
         }
-    }
 
-    if (player2Points >= 4) {
         if (player2Points > player1Points) {
-            let playerName = "2";
-
-            if (delta > 1) {
-                return `Win for player ${playerName}`;
-            } else if (delta === 1) {
-                return `Advantage player ${playerName}`;
-            } else {
-                return;
-            }
+            return decideWinner(player1Points, player2Points);
         }
     }
-
 
     if (player2Points === player1Points) {
         return (player1Points >= 3 ? "Deuce" : scoreFor(player1Points) + "-All");
     }
 
     return scoreFor(player1Points) + "-" + scoreFor(player2Points);
+}
+
+function decideWinner(player1Points, player2Points) {
+    const delta = Math.abs(player1Points - player2Points);
+    const playerName = (player1Points > player2Points ? "1" : "2");
+
+    if (delta > 1) {
+        return `Win for player ${playerName}`;
+    }
+    if (delta === 1) {
+        return `Advantage player ${playerName}`;
+    }
 }
 
 function scoreFor(points) {
