0d5e37c9bea4bec515881530618f358475824e15
andys <andysmith836@gmail.com>
2020-04-15 22:26:58 +0100

    Remove duplication

--- TennisScoreCalculator.js
+++ TennisScoreCalculator.js
@@ -3,12 +3,16 @@ function score(player1Points, player2Points) {
     let maxScore = Math.max(player1Points, player2Points);
 
     if (maxScore >= 4) {
-        if (player1Points > player2Points) {
-            return decideWinner(player1Points, player2Points);
-        }
+        if (player1Points !== player2Points) {
+            const delta = Math.abs(player1Points - player2Points);
+            const playerName = (player1Points > player2Points ? "1" : "2");
 
-        if (player2Points > player1Points) {
-            return decideWinner(player1Points, player2Points);
+            if (delta > 1) {
+                return `Win for player ${playerName}`;
+            }
+            if (delta === 1) {
+                return `Advantage player ${playerName}`;
+            }
         }
     }
 
@@ -17,18 +21,6 @@ function score(player1Points, player2Points) {
     }
 
     return scoreFor(player1Points) + "-" + scoreFor(player2Points);
-}
-
-function decideWinner(player1Points, player2Points) {
-    const delta = Math.abs(player1Points - player2Points);
-    const playerName = (player1Points > player2Points ? "1" : "2");
-
-    if (delta > 1) {
-        return `Win for player ${playerName}`;
-    }
-    if (delta === 1) {
-        return `Advantage player ${playerName}`;
-    }
 }
 
 function scoreFor(points) {
