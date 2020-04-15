497865943e23030151c3861a6976d8efd1af86fc
andys <andysmith836@gmail.com>
2020-04-15 21:29:07 +0100

    Clean up tests

--- TennisScoreCalculator.spec.js
+++ TennisScoreCalculator.spec.js
@@ -2,7 +2,6 @@ const score = require("./TennisScoreCalculator").score;
 
 describe("Tennis Calculator", () => {
     it("should score simple scores", () => {
-        expect(score(0, 0)).toEqual("Love-All");
         expect(score(1, 0)).toEqual("Fifteen-Love");
         expect(score(0, 1)).toEqual("Love-Fifteen");
         expect(score(2, 0)).toEqual("Thirty-Love");
@@ -11,6 +10,12 @@ describe("Tennis Calculator", () => {
         expect(score(1, 2)).toEqual("Fifteen-Thirty");
         expect(score(3, 0)).toEqual("Forty-Love");
         expect(score(0, 3)).toEqual("Love-Forty");
+    });
+
+    it("should score ties", () => {
+        expect(score(0, 0)).toEqual("Love-All");
+        expect(score(1, 1)).toEqual("Fifteen-All");
+        expect(score(2, 2)).toEqual("Thirty-All");
 
     });
 });
